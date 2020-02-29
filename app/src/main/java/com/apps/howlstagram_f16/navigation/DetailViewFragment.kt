da6859344e37852bba5c320ea006e167379de514
Seong-Hwan <ksh970516@pukyong.ac.kr>
2020-02-29 15:22:22 +0900

    Implement like button

--- app/src/main/java/com/apps/howlstagram_f16/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/apps/howlstagram_f16/navigation/DetailViewFragment.kt
@@ -10,15 +10,20 @@ import androidx.recyclerview.widget.RecyclerView
 import com.apps.howlstagram_f16.R
 import com.apps.howlstagram_f16.navigation.model.ContentDTO
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_detail.view.*
 import kotlinx.android.synthetic.main.item_detail.view.*
 
 class DetailViewFragment : Fragment() {
     var firestore : FirebaseFirestore? = null
+    var uid : String? = null
+
+
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
         firestore = FirebaseFirestore.getInstance()
+        uid = FirebaseAuth.getInstance().currentUser?.uid
 
         view.detailviewfragment_recycleview.adapter = DetailViewRecyclerViewAdapter()
         view.detailviewfragment_recycleview.layoutManager = LinearLayoutManager(activity)
@@ -65,7 +70,32 @@ class DetailViewFragment : Fragment() {
 
             viewholder.detailviewitem_favoritecounter_textview.text = "Likes " + contentDTOs!![p1].favoriteCount
 
-            Glide.with(p0.itemView.context).load(contentDTOs!![p1].imageUrl).into(viewholder.detailviewitem_profile_image)
+            viewholder.detailviewitem_favorite_imageview.setOnClickListener {
+                favoriteEvent(p1)
+            }
+
+            if(contentDTOs!![p1].favorites.containsKey(uid)) {
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite)
+            } else {
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite_border)
+            }
+        }
+
+        fun favoriteEvent(position : Int) {
+            var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction{ transaction ->
+
+                var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
+
+                if(contentDTO!!. favorites.containsKey(uid)) {
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount?.minus(1)
+                    contentDTO?.favorites.remove(uid)
+                } else {
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount?.plus(1)
+                    contentDTO?.favorites[uid!!] = true
+                }
+                transaction.set(tsDoc, contentDTO)
+            }
         }
 
     }
